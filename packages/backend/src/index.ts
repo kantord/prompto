9db5c941d3d4d95f948af7e73de87d3e7d584aaa
Daniel Kantor <git@daniel-kantor.com>
2025-05-12 09:05:22 +0200

    add web socket server

--- packages/backend/src/index.ts
+++ packages/backend/src/index.ts
@@ -1,6 +1,11 @@
-import { createHTTPServer } from '@trpc/server/adapters/standalone';
+import { applyWSSHandler } from '@trpc/server/adapters/ws';
+import ws from 'ws';
 import { publicProcedure, router } from './trpc';
 import { z } from 'zod';
+
+const wss = new ws.Server({
+  port: 3011,
+});
 
 const appRouter = router({
   ping: publicProcedure
@@ -10,13 +15,36 @@ const appRouter = router({
     }),
 });
 
+const handler = applyWSSHandler({
+  wss,
+  router: appRouter,
+  // Enable heartbeat messages to keep connection open (disabled by default)
+  keepAlive: {
+    enabled: true,
+    // server ping message interval in milliseconds
+    pingMs: 30000,
+    // connection is terminated if pong message is not received in this many milliseconds
+    pongWaitMs: 5000,
+  },
+});
+
+
+
 // Export type router type signature,
 // NOT the router itself.
 export type AppRouter = typeof appRouter;
 
 
-const server = createHTTPServer({
-  router: appRouter,
+wss.on('connection', (ws) => {
+  console.log(`➕➕ Connection (${wss.clients.size})`);
+  ws.once('close', () => {
+    console.log(`➖➖ Connection (${wss.clients.size})`);
+  });
 });
- 
-server.listen(3011);
+
+console.log('✅ WebSocket Server listening on ws://localhost:3011');
+wss.on('SIGTERM', () => {
+  console.log('SIGTERM');
+  handler.broadcastReconnectNotification();
+  wss.close();
+});
